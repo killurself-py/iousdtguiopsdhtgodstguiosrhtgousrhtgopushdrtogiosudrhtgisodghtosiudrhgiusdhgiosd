60a1690955007aac9798046d46651712049c9a6c
killurself-py <lztjwongu@gmail.com>
2024-09-08 19:59:15 +0500

    Update 777.py

--- 777.py
+++ 777.py
@@ -7,7 +7,6 @@ bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 @bot.command()
 async def attack(ctx, ip, protocol, method, time, cps):
     subprocess.Popen(f"java -jar bot.jar {ip} {protocol} {method} {time} {cps}", shell=True)
-    await ctx.send("+conc")
 @bot.command()
 async def stop(ctx):
     subprocess.Popen(f"pkill java", shell=True)
