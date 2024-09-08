f1cee314c49f6a5329742c2c836cfab84dd83758
killurself-py <lztjwongu@gmail.com>
2024-09-08 17:59:24 +0500

    Add files via upload

--- 777.py
+++ 777.py
@@ -16,4 +16,7 @@ async def stop(ctx):
 async def proxy(ctx, link):
     open("proxies.txt", "w").write(requests.get(link).text)
     await ctx.send("+updated")
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pinged!")
 bot.run(base64.b64decode("TVRJM056ZzVOamsyTVRrME5EY3hPVE0yTUEuR1NNUzdILnNaXzFBM0tKLVRtM193VkI2SElqOXdpX2k5NmJyd2tZVEFaN3RR").decode())
